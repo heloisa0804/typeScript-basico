3110b6f268c91feca440e6013bcc546925623316
heloisa0804 <heloisa.germanobussi@yahoo.com.br>
2025-06-17 09:38:19 -0300

    Tipos basicos
    number e string de exemplos

---
+++ 02-tipagem-basica.ts
@@ -0,0 +1,13 @@
+export {};
+
+// Exemplos de tipos básicos
+let idade: number = 30;
+let ativo: boolean = true;
+let nome: string = "Beltrano";
+
+console.log(`${nome} tem ${idade} anos.`);
+
+// Nova atribuição
+idade = 31;
+console.log(`${nome} agora tem ${idade} anos.`);
+console.log(`Status: ${ativo ? "Ativo" : "Inativo"}`);
