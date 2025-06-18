708e008e02f6474c3274a7e809fe61e6f115dce3
heloisa0804 <heloisa.germanobussi@yahoo.com.br>
2025-06-18 08:42:21 -0300

    arrays.ts

--- 05-arrays.ts
+++ 05-arrays.ts
@@ -1,3 +1,4 @@
+import { separador } from "./modulos";
 export {};
 
 // Sintaxe 1
@@ -10,3 +11,19 @@ console.log(bandas);
 const artista: string[] = ["Jon Oliva", "Sting", "David Gilmour"];
 artista.push("Neil Peart");
 console.log(artista);
+
+separador();
+
+// Arrays com tipos mistos ou combinados
+const pessoas: (string | number)[] = ["Tiago", 28, "Maria", 45];
+console.log(pessoas);
+
+separador();
+
+const linguagens: string[][] = [
+  ["JS", "TS"],
+  ["HTML", "CSS", "Bootstrap"],
+];
+
+console.log(linguagens[0][0]);
+console.log(linguagens[1][0]);
--- dist/05-arrays.js
+++ dist/05-arrays.js
@@ -1,8 +1,19 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+const modulos_1 = require("./modulos");
 const bandas = ["Metalica", "Slayer", "Iron Maiden"];
 bandas.push("NightWhish");
 console.log(bandas);
 const artista = ["Jon Oliva", "Sting", "David Gilmour"];
 artista.push("Neil Peart");
 console.log(artista);
+(0, modulos_1.separador)();
+const pessoas = ["Tiago", 28, "Maria", 45];
+console.log(pessoas);
+(0, modulos_1.separador)();
+const linguagens = [
+    ["JS", "TS"],
+    ["HTML", "CSS", "Bootstrap"],
+];
+console.log(linguagens[0][0]);
+console.log(linguagens[1][0]);
